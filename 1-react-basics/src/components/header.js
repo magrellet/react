3aa12a6548179580810032fa372ec0bf9c763c0a
Martin <martin.alejandro.grellet@gmail.com>
2018-11-04 19:04:51 -0300

    Initial components and styles

---
+++ 1-react-basics/src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className="logo">Logo</div>
+                <input></input>
+            </header>
+        )
+    }
+}
+
+export default Header;
